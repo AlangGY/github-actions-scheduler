8b36c071ca2a83656970f995e910f057d2230e41
Alang Kim <rlaangh77@naver.com>
2022-11-02 10:55:33 +0900

    fix jq command 사용하지 않고 동일한 로직 작성

--- pr-day-reducer.sh
+++ pr-day-reducer.sh
@@ -2,36 +2,24 @@
 pullRequests="$(gh api graphql -F owner=AlangGY -F name=github-actions-scheduler -f query='
             query($name: String!, $owner: String!) {
               repository(owner: $owner, name: $name) {
-                pullRequests(last:100) {  nodes { number, title, labels(last: 20) { nodes { name } }  } }
+                pullRequests(last:100) {  nodes { number , labels(last: 20) { nodes { name } }  } }
               }
             }
-          ' --jq '.data.repository.pullRequests.nodes')"
+          ' --jq '[(.data.repository.pullRequests.nodes.[] | "@", .number , .labels.nodes[].name)] | join("_")')"
 
-echo $pullRequests
-
-for pr in $(echo $pullRequests | jq -r '.[] | @base64'); do
-    _jq() {
-        echo ${pr} | base64 --decode | jq -r ${1}
-    }
-
-    number=$(_jq '.number')
-    title=$(_jq '.title')
-    labels=$(_jq '.labels.nodes')
-    for label in $(echo $labels | jq -r '.[] | @base64'); do
-        _jq() {
-            echo ${label} | base64 --decode | jq -r ${1}
-        }
-        label=$(_jq '.name')
-        echo $label
-        if [[ $label =~ ["^D."] ]]; then
-            echo "id: $number, title: $title, label: $label"
-            if [[ $label == "D-0" ]]; then
-                nextLabel="expired"
-            else
-                nextLabel="D"-$((${label:2} - 1))
-            fi
-            $(gh pr edit $number --remove-label $label --add-label $nextLabel)
-            echo $label '->' $nextLabel
+for pr in $(echo $pullRequests | sed 's/_@_/\n/g' | sed 's/@_/\n/g'); do
+  prNumber=$(echo $pr | cut -d'_' -f1)
+  prLabels=$(echo $pr | cut -d'_' -f2-)
+  for label in $(echo $prLabels | sed 's/_/\n/g'); do
+    if [[ $label =~ ["^D."] ]]; then
+        if [[ $label == "D-0" ]]; then
+            nextLabel="expired"
+        else
+            nextLabel="D"-$((${label:2} - 1))
         fi
-    done    
+        echo "prNumber:" $prNumber
+        echo $label '->' $nextLabel
+        echo $(gh pr edit $prNumber --remove-label $label --add-label $nextLabel)
+    fi
+  done
 done
