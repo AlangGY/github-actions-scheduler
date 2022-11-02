71a58bf217c2c893d46bb0d88adf9b7422af1280
Alang Kim <rlaangh77@naver.com>
2022-11-02 11:27:15 +0900

    fix: log 출력 수정 및 regex 수정

--- pr-day-reducer.sh
+++ pr-day-reducer.sh
@@ -11,14 +11,13 @@ for pr in $(echo $pullRequests | sed 's/_@_/\n/g' | sed 's/@_/\n/g'); do
   prNumber=$(echo $pr | cut -d'_' -f1)
   prLabels=$(echo $pr | cut -d'_' -f2-)
   for label in $(echo $prLabels | sed 's/_/\n/g'); do
-    if [[ $label =~ ["^D."] ]]; then
+    if [[ $label =~ ["^(D-)"] ]]; then
         if [[ $label == "D-0" ]]; then
             nextLabel="expired"
         else
             nextLabel="D"-$((${label:2} - 1))
         fi
-        echo "prNumber:" $prNumber
-        echo $label '->' $nextLabel
+        echo "change prNumber:" $prNumber "'s label from" $label "to" $nextLabel
         echo $(gh pr edit $prNumber --remove-label $label --add-label $nextLabel)
     fi
   done
